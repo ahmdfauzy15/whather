373a10b00b86f2506246b56c8a561c6b609deaa1
ahmdfauzy15 <ahfauzy15@gmail.com>
2024-03-04 10:54:49 +0700

    git init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  weatherTemp: any
+  todayDate = new Date()
+  cityName = ''
+  weatherIcon: any
+  weatherDetails: any
+  name = ''
+  loading = true
 
-  constructor() {}
 
+
+  constructor(private httpClient: HttpClient) {
+    this.loadData();
+  }
+
+  loadData() {
+    const API_URL = environment.API_URL;
+    const API_KEY = environment.API_KEY;
+
+    this.httpClient.get(`${API_URL}/weather?q=${this.cityName}&appid=${API_KEY}`).subscribe((results: any) => {
+      console.log(results);
+      if (results && results.main && results.name) {
+        this.weatherTemp = results.main;
+        this.name = results.name;
+        this.weatherDetails = results.weather[0];
+        this.weatherIcon = `https://openweathermap.org/img/wn/${this.weatherDetails.icon}@2x.png`
+        this.loading = false
+        console.log(this.weatherTemp);
+        console.log(this.cityName);
+        console.log(this.weatherDetails);
+        console.log(this.weatherIcon);
+
+
+
+      } else {
+        console.error('Failed to fetch weather data');
+      }
+    });
+  }
 }
